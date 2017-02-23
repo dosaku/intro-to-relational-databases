2e7618f8985633e2ce47a612139e6c2a171e5ed2
Ryan Minniear <rjminniear@gmail.com>
2017-02-22 21:21:57 -0800

    feat: make count test pass
    Implemented deleteMatches(), deletePlayers(), countPlayers(), and
    registerPlayer().

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -6,4 +6,29 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+drop table if exists Player, Match;
 
+create table Player
+    (id serial primary key,
+     name text);
+
+create table Match
+    (id1 integer references Player (id),
+     id2 integer references Player (id),
+     winner_id serial references Player(id),
+     primary key (id1, id2),
+     check (id1 != id2 and
+            (winner_id = id1 or winner_id = id2))
+     );
+
+-- sample data to work with during dev
+insert into Player (name) values ('Ryan');
+insert into Player (name) values ('Allen');
+insert into Player (name) values ('Jon');
+insert into Player (name) values ('Laura');
+insert into Player (name) values ('Cam');
+insert into Player (name) values ('Cammie');
+
+insert into Match values (1, 2, 2);
+insert into Match values (3, 4, 3);
+insert into Match values (5, 6, 6);
